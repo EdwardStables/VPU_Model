8e171d24b65235eb29bcc32597e164c044304381
Edward Stables <edward.stables1198@gmail.com>
2024-07-14 00:24:49 +0100

    dump register state

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #include "manager_core.h"
 
 void initialise_state(vpu::config::Config& config, std::unique_ptr<vpu::mem::Memory>& memory) {
-    vpu::mem::Snooper::copy_file_in(memory, config.input_file);
+    vpu::mem::MemorySnooper::copy_file_in(memory, config.input_file);
 };
     
 void dump_program(std::unique_ptr<vpu::mem::Memory>& memory){
@@ -19,6 +19,22 @@ void dump_program(std::unique_ptr<vpu::mem::Memory>& memory){
         vpu::defs::Opcode opcode = vpu::defs::get_opcode(data);
         std::cout << std::setfill('0') << std::setw(8) << std::hex << i*4;
         std::cout << " " << vpu::defs::opcode_to_string(opcode) << std::endl;
+    }
+}
+
+void dump_regs(vpu::config::Config& config, vpu::core::ManagerCore& core) {
+    fs::path dump_path = config.dump_regs;
+    if (fs::exists(dump_path) && fs::is_directory(dump_path)) {
+        std::cerr << "Error: Dump path " << config.dump_regs << " is a directory. Give a file name.";
+        exit(1);
+    }
+
+    std::cout << "Dumping register state to " << config.dump_regs << std::endl;
+    std::ofstream dump(dump_path, std::ios::out);
+    for (uint8_t r = 0; r < vpu::defs::REGISTER_COUNT; r++) {
+        dump << vpu::defs::register_to_string((vpu::defs::Register)r) << " ";
+        dump << vpu::core::ManagerCoreSnooper::get_register(core,(vpu::defs::Register)r);
+        dump << "\n";
     }
 }
 
@@ -60,6 +76,10 @@ int main(int argc, char *argv[]) {
 
     vpu::core::ManagerCore core(config, memory);
     run_program(core, config);
+
+    if (config.dump_regs != "") {
+        dump_regs(config, core);
+    }
 
     return 0;
 }
