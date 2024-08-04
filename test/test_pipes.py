b21345963d10f86cb660dcd1e79386f28f04f345
Edward Stables <edward.stables1198@gmail.com>
2024-08-04 20:23:59 +0100

    slightly genericise dma tests and add copy test

--- test/test_pipes.py
+++ test/test_pipes.py
@@ -12,7 +12,24 @@ def params(prog):
 
 @pytest.mark.parametrize("run_program, actual_memory", params("dma_set"), indirect=True)
 def test_dma_set(run_program,actual_memory):
-    assert actual_memory[0xF00000-1] == 0
-    for offset in range(0x10000):
-        assert actual_memory[0xF00000+offset] == 0xFF
-    assert actual_memory[0xF10000+1] == 0
+    base = 0xF00000
+    length = 0x10000
+    assert actual_memory[base-1] == 0
+    for offset in range(length):
+        assert actual_memory[base+offset] == 0xFF
+    assert actual_memory[base+length] == 0
+
+@pytest.mark.parametrize("run_program, actual_memory", params("dma_copy"), indirect=True)
+def test_dma_copy(run_program,actual_memory):
+    base = 0xF00000
+    length = 0x10000
+    assert actual_memory[base-1] == 0
+    for offset in range(length):
+        assert actual_memory[base+offset] == 0xFF
+    assert actual_memory[base+length] == 0
+
+    base = 0xF70000
+    assert actual_memory[base-1] == 0
+    for offset in range(length):
+        assert actual_memory[base+offset] == 0xFF
+    assert actual_memory[base+length] == 0
